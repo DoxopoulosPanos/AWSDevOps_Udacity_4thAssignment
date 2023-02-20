91cbfeaa63405058e8d15b179763cd5bf1b93e01
EC2 Default User <ec2-user@ip-172-31-31-164.eu-central-1.compute.internal>
2023-02-20 15:24:18 +0000

    complete task 4

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=panosdoxos/prediction_api
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag prediction_api:latest panosdoxos/prediction_api:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push panosdoxos/prediction_api:latest
