573f8f25f855e48e03889f3d193d1d9ff9dc5f95
EC2 Default User <ec2-user@ip-172-31-31-164.eu-central-1.compute.internal>
2023-02-21 13:08:11 +0000

    complete run_kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=panosdoxos/prediction_api
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+minikube kubectl -- run predictionapp --image=panosdoxos/prediction_api --port=80
 
 
 # Step 3:
 # List kubernetes pods
+minikube kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+minikube kubectl port-forward predictionapp 8000:80
 
